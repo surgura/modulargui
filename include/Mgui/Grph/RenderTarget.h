35c8359c623cb0fe72320500bbe2f413b4296524
Aart Stuurman <aartstuurman@hotmail.com>
2015-08-16 12:23:22 +0200

    Create RenderTarget.h

---
+++ include/Mgui/Grph/RenderTarget.h
@@ -0,0 +1,14 @@
+/// This Source Code Form is subject to the terms of the Mozilla Public
+/// License, v. 2.0. If a copy of the MPL was not distributed with this
+/// file, You can obtain one at http://mozilla.org/MPL/2.0/.
+
+/// @author Aart Stuurman
+
+// Header guard
+#ifndef MGUI_RENDER_TARGET_H
+#define MGUI_RENDER_TARGET_H
+
+
+
+// End header guard
+#endif // MGUI_RENDER_TARGET_H
